fdf524ab4196f37062fe746175d1277234939d77
sherlockouo <myqq12345@outlook.com>
2021-07-10 15:17:01 +0800

    lab2

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -12,10 +12,67 @@ using namespace std;
 
 //receive segments 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    // DUMMY_CODE(seg);
-    
+    uint64_t prev_abs_ackno = 0;
+    //处理第一个 syn
+    if(seg.header().syn && !isn.has_value()){
+        //如果header 中有 syn 且 当前 isn 为空 就可以设置 isn 为
+        isn = make_optional<WrappingInt32>(seg.header().seqno);
+    }
+    //如果isn 有值 那么 就有
+    if(isn.has_value()){
+        prev_abs_ackno = abs_ackno();
+    }else{
+        return ;
+    }
+
+    uint64_t checkpoint = _reassembler.stream_out().bytes_written();
+
+    //将seqno 包装为 abs_seq
+    uint64_t abs_seq = unwrap(seg.header().seqno,isn.value(),checkpoint);
+
+    uint64_t prev_window_size = window_size();
+
+    //ACK after FIN received
+    if(abs_fin_seq && abs_seq >= abs_fin_seq && seg.length_in_sequence_space()==0){
+        
+        return ;
+    }
+
+    if(!(abs_seq < prev_abs_ackno + prev_window_size && abs_seq + seg.length_in_sequence_space() > prev_abs_ackno)){
+        //dont know how
+        return ;
+    }
+
+    bool all_filled = abs_seq + seg.length_in_sequence_space() <= prev_abs_ackno + prev_window_size;
+
+    if(all_filled && seg.header().fin){
+        abs_fin_seq = abs_seq + seg.length_in_sequence_space();
+    }
+
+
+    uint64_t stream_indices = abs_seq > 0?abs_seq - 1: 0;
+    string payload (seg.payload().copy());
+    _reassembler.push_substring(payload,stream_indices,stream_indices+seg.payload().size() + 2==abs_fin_seq);
+
+    return ;
+
+
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+uint64_t TCPReceiver::abs_ackno() const{
+    uint64_t abs_ackno_without_fin = 1 + _reassembler.stream_out().bytes_written();
+    //如果获取到fin了 那么就结束了 返回ackno的绝对位置
+    if(abs_ackno_without_fin + 1 == abs_fin_seq){
+        return abs_ackno_without_fin+1;
+    }
+    return abs_ackno_without_fin;
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { 
+    if(!isn.has_value()){
+        return nullopt;
+    }
+    return make_optional<WrappingInt32>(wrap(abs_ackno(),isn.value())); 
+}
+
+size_t TCPReceiver::window_size() const { return _reassembler.stream_out().remaining_capacity(); }
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -8,6 +8,8 @@
 
 #include <optional>
 
+using namespace std;
+
 //! \brief The "receiver" part of a TCP implementation.
 
 //! Receives and reassembles segments into a ByteStream, and computes
@@ -31,7 +33,7 @@ class TCPReceiver {
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity),isn(std::nullopt),abs_fin_seq(0) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
